bc02d67ee351dcebe9d34274f38fe84973649265
trungem <nguyentrungem2002@gmail.com>
2023-11-24 21:50:52 +0700

    Cấu hình Router/Layout cho dự án

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layouts';
+
 function App() {
     return (
-        <div className="App">
-            <h1>HELLO AE</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
