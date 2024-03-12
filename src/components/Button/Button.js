0eb2ffe04e2526d6f9d36442fe9fdd8b8069fd96
Trung Em ^_^ <tem2k2.dev@gmail.com>
2024-03-12 09:53:56 +0700

    Giải quyết vấn đề nhiều file index.js

---
+++ src/components/Button/Button.js
@@ -0,0 +1,88 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import classNames from 'classnames/bind';
+
+import styles from './Button.module.scss';
+
+const cx = classNames.bind(styles);
+
+function Button({
+    to,
+    href,
+    primary = false,
+    outline = false,
+    text = false,
+    rounded = false,
+    disabled = false,
+    small = false,
+    large = false,
+    onClick,
+    children,
+    className,
+    leftIcon,
+    rightIcon,
+    ...passProps
+}) {
+    let Comp = 'button';
+
+    const props = {
+        onClick,
+        ...passProps,
+    };
+
+    if (disabled) {
+        // delete props.onClick;
+
+        Object.keys(props).forEach((key) => {
+            if (key.startsWith('on') && typeof props[key] === 'function') {
+                delete props[key];
+            }
+        });
+    }
+
+    if (href) {
+        Comp = 'a';
+        props.href = href;
+    } else if (to) {
+        Comp = Link;
+        props.to = to;
+    }
+
+    const classes = cx('wrapper', {
+        [className]: className,
+        primary,
+        outline: outline,
+        text,
+        rounded,
+        disabled,
+        small,
+        large,
+    });
+
+    return (
+        <Comp className={classes} {...props}>
+            {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
+            <span className={cx('title')}>{children}</span>
+            {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
+        </Comp>
+    );
+}
+
+Button.propTypes = {
+    to: PropTypes.string,
+    href: PropTypes.string,
+    primary: PropTypes.bool,
+    outline: PropTypes.bool,
+    text: PropTypes.bool,
+    rounded: PropTypes.bool,
+    disabled: PropTypes.bool,
+    small: PropTypes.bool,
+    large: PropTypes.bool,
+    children: PropTypes.string,
+    className: PropTypes.node.isRequired,
+    leftIcon: PropTypes.node,
+    rightIcon: PropTypes.node,
+    onClick: PropTypes.func,
+};
+
+export default Button;
---
+++ src/layouts/components/Header/Header.js
@@ -0,0 +1,160 @@
+import { Link } from 'react-router-dom';
+import classNames from 'classnames/bind';
+import 'tippy.js/dist/tippy.css';
+import Tippy from '@tippyjs/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
+
+import config from '~/config';
+import Button from '~/components/Button';
+import styles from './Header.module.scss';
+import images from '~/assets/images';
+import Menu from '~/components/Popper/Menu';
+import {
+    FeedbackIcon,
+    GetCoinsIcon,
+    InboxIcon,
+    KeyboardIcon,
+    LanguageIcon,
+    LogoutIcon,
+    MessagesIcon,
+    ProfileIcon,
+    SettingIcon,
+    UploadIcon,
+} from '~/components/Icons';
+import Image from '~/components/Image';
+import Search from '../Search';
+
+const cx = classNames.bind(styles);
+const MENU_ITEMS = [
+    {
+        title: 'English',
+        icon: <LanguageIcon width="2rem" height="2rem" />,
+        children: {
+            title: 'Language',
+            data: [
+                {
+                    type: 'language',
+                    code: 'en',
+                    title: 'English',
+                },
+                {
+                    type: 'language',
+                    code: 'vi',
+                    title: 'Tiếng việt',
+                },
+            ],
+        },
+    },
+    {
+        title: 'Feedback and held',
+        icon: <FeedbackIcon width="2rem" height="2rem" />,
+        to: '/feedback',
+    },
+    {
+        title: 'Keyboard shortcuts',
+        icon: <KeyboardIcon width="2rem" height="2rem" />,
+    },
+];
+
+function Header() {
+    const currentUser = true;
+
+    const handleMenuChange = (menuItem) => {
+        switch (menuItem) {
+            case 'language':
+                //Handle change language
+                break;
+            default:
+        }
+    };
+
+    const userMenu = [
+        {
+            title: 'View profile',
+            icon: <ProfileIcon width="2rem" height="2rem" />,
+            to: '/@hoaa',
+        },
+        {
+            title: 'Get coins',
+            icon: <GetCoinsIcon width="2rem" height="2rem" />,
+            to: '/coin',
+        },
+        {
+            title: 'Settings',
+            icon: <SettingIcon width="2rem" height="2rem" />,
+            to: '/settings',
+        },
+        ...MENU_ITEMS,
+        {
+            title: 'Log out',
+            icon: <LogoutIcon width="2rem" height="2rem" />,
+            to: '/logout',
+            separate: true,
+        },
+    ];
+
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <Link to={config.routes.home} className={cx('logo-link')}>
+                    <img src={images.logo} alt="TikTok" />
+                </Link>
+
+                <Search />
+
+                <div className={cx('action')}>
+                    {currentUser ? (
+                        <>
+                            <Button
+                                to={'/upload'}
+                                children="Upload"
+                                leftIcon={<UploadIcon width="2rem" height="2rem" />}
+                                outline
+                                className="btn-upload"
+                            />
+                            <Tippy delay={[0, 100]} content="Messages">
+                                <button className={cx('action-btn')}>
+                                    <MessagesIcon />
+                                </button>
+                            </Tippy>
+                            <Tippy delay={[0, 100]} content="Inbox">
+                                <button className={cx('action-btn')}>
+                                    <InboxIcon width="3.2rem" height="3.2rem" />
+                                    <span className={cx('badge')}>12</span>
+                                </button>
+                            </Tippy>
+                        </>
+                    ) : (
+                        <>
+                            <Button
+                                to={'/upload'}
+                                children="Upload"
+                                leftIcon={<UploadIcon width="2rem" height="2rem" />}
+                                outline
+                                className="btn-upload"
+                            />
+                            <Button primary>Log in</Button>
+                        </>
+                    )}
+                    <Menu items={currentUser ? userMenu : MENU_ITEMS} onChange={handleMenuChange}>
+                        {currentUser ? (
+                            <Image
+                                className={cx('user-avatar')}
+                                src="https://p9-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/ceb7cb3459ea9b0b9dcb190806cf6d06.jpeg?lk3s=a5d48078&x-expires=1709737200&x-signature=%2FkD%2B5NI9rLt6gsQW2MRHbWK6EPs%3D"
+                                fallback="https://fullstack.edu.vn/static/media/f8-icon.18cd71cfcfa33566a22b.png"
+                                alt=""
+                            />
+                        ) : (
+                            <button className={cx('more-btn')}>
+                                <FontAwesomeIcon icon={faEllipsisVertical} />
+                            </button>
+                        )}
+                    </Menu>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
